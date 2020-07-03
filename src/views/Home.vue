994092e9129effea17fb4f8a44f237c13b5e53a2
nurfiansyah06 <nurfiansyah06@gmail.com>
2020-07-03 16:55:34 +0700

    projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderOlshop />
+    <HeroOlshop />
+    <BannerOlshop />    
+    <InstaOlshop />
+    <PartnerOlshop />
+    <FooterOlshop />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderOlshop from '@/components/HeaderOlshop.vue'
+import HeroOlshop from '@/components/HeroOlshop.vue'
+import BannerOlshop from '@/components/BannerOlshop.vue'
+import InstaOlshop from '@/components/InstaOlshop.vue'
+import PartnerOlshop from '@/components/PartnerOlshop.vue'
+import FooterOlshop from '@/components/FooterOlshop.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderOlshop,
+    HeroOlshop,
+    BannerOlshop,
+    InstaOlshop,
+    PartnerOlshop,
+    FooterOlshop
+  }
+}
+</script>
